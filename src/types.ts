8880054bbf9819473b1732a6751aa19eabb6aae5
Tim Zook <tim@zookatron.com>
2019-09-02 16:32:51 -0500

    hotfix for "Type instantiation is excessively deep and possibly infinite."

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-export type Prefer<P, T> = P & Omit<T, keyof P>;
+export type Prefer<P, T> = P & T;
 
 // https://stackoverflow.com/questions/50374908/transform-union-type-to-intersection-type/50375286#50375286
 export type UnionToIntersection<U> = (U extends any
